7d02f64359c7f80b50df158f9d65f4bb3454a099
mokimarman8-star <mokimarman8@gmail.com>
2026-08-21 12:28:35 +0530

    Add root build.gradle configuration

---
+++ android/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+
+task("clean") {
+    delete(rootProject.buildDir)
+}
